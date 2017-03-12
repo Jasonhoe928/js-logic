ab13fa0e43895a29f15ecfceaff02da99a21856b
Jason <Jason@Colins-MacBook-Pro.local>
2017-03-11 23:04:09 -1000

    Completed #6

--- exercises.js
+++ exercises.js
@@ -133,7 +133,14 @@ console.log(bigBird("kek"));
  * Console.log your result.
 */
 
-
+function isEqual(first, second){
+  if (first === second){
+    return "You look mahvelous!";
+  } else {
+    return "I don't know who you are anymore.";
+  }
+}
+console.log(isEqual("hi", "hi"));
 /*
  * #7
  * Function - notEqual
